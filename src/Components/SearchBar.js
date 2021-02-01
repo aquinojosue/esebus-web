1708fc54883e8448acdddeb9c14650f33c26d82b
Josue Aquino <aquinojosue>
2021-02-01 09:29:57 -0500

    fix searchbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,7 +6,7 @@ function SearchBar(props){
     }
     return(
      <div class="pt-2 relative mx-auto text-gray-600">
-        <input onChange={(e)=>setValue(e.target.value)} value={props.searchFilter} class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none shadow-md focus:border-esebus-dark"
+        <input onChange={(e)=>setValue(e.target.value)} value={props.searchFilter} class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none shadow-md focus:border-esebus-dark w-full"
           type="search" name="search" placeholder="Search"/>
       </div>
     )
