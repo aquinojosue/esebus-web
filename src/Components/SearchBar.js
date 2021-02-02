e5d847d6daf90eaea3e4bb41dc97ea84bca2093e
Josue Aquino <aquinojosue>
2021-02-02 00:15:01 -0500

    code format

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,8 +6,15 @@ function SearchBar(props){
     }
     return(
      <div class="pt-2 relative mx-auto text-gray-600">
-        <input onChange={(e)=>setValue(e.target.value)} value={props.searchFilter} class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none shadow-md focus:border-esebus-dark w-full"
-          type="search" name="search" placeholder="Search"/>
+        <input 
+            onChange={(e)=>setValue(e.target.value)} 
+            value={props.searchFilter}
+            class={
+                "border-2 border-gray-300 bg-white h-10 px-5 pr-16 " +
+                "rounded-lg text-sm focus:outline-none shadow-md " +
+                "focus:border-esebus-dark w-full"
+            }
+            type="search" name="search" placeholder="Search"/>
       </div>
     )
 }
