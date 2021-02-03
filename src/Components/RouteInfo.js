1ec9119d3ff0e9c1d06bef08e7c515dfe170a70b
Josue Aquino <aquinojosue>
2021-02-03 10:19:13 -0500

    added transition effects routeinfo

--- src/Components/RouteInfo.js
+++ src/Components/RouteInfo.js
@@ -1,22 +1,27 @@
 import React from 'react'
-
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
 function RouteInfo(props){
     return(
         <div className="leaflet-control overflow-y-auto scrolling-touch w-full">
+            <TransitionGroup>
             {
-                props.routes.map((route, index)=>{
+                props.routes.filter(route => route.shown).map((route, index)=>{
                     return(
-                        (route.shown)?
-                        <React.Fragment key={index}>
-                            <div class="h-auto w-full border border-gray-500 p-5 mb-2 shadow-md">
-                                <div class="row-span-3"><h1 className="text-gray-700 font-bold text-lg">{route.nombreRuta}</h1></div>
-                                <div class="col-span-2">Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
-                                <div class="row-span-2 col-span-2">Vuelta <hr style={{"height":"5px", "background":route.colorVuelta}}/></div>
-                            </div>
-                        </React.Fragment>:""
+                        <CSSTransition timeout={(index + 1)*200} classNames="item">
+                                <div class="h-auto w-full border border-gray-500 p-5 mb-2 shadow-md" key={index}>
+                                    <div class="row-span-3"><h1 className="text-gray-700 font-bold text-lg">{route.nombreRuta}</h1></div>
+                                    <div class="col-span-2">Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
+                                    <div class="row-span-2 col-span-2">Vuelta <hr style={{"height":"5px", "background":route.colorVuelta}}/></div>
+                                </div>
+                        </CSSTransition>
                     )
                 })
             }
+            </TransitionGroup>
+            
         </div>
     );
 }
