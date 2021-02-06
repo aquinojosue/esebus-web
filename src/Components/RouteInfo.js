21040c16b5a7bdf7db8d111ae4adbdf4798fb102
Josué Aquino <aquinojosue3@gmail.com>
2021-02-06 21:32:59 +0000

    talvez ultimos cambios a la leyenda

--- src/Components/RouteInfo.js
+++ src/Components/RouteInfo.js
@@ -11,10 +11,10 @@ function RouteInfo(props){
                 props.routes.map((route, index)=>{
                     return(
                         <CSSTransition timeout={(index + 1)*300} classNames="item">
-                                <div class="h-auto w-full border border-gray-500 p-5 mb-2 shadow-md" key={index}>
-                                    <div class="row-span-3"><h1 className="text-gray-700 font-bold text-lg">{route.nombreRuta}</h1></div>
-                                    <div class="col-span-2">Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
-                                    <div class="row-span-2 col-span-2">Vuelta <hr style={{"height":"5px", "background":route.colorVuelta}}/></div>
+                                <div class="h-25 bg-white w-full border border-gray-500 p-3 mb-2 shadow-lg" key={index}>
+                                    <div><h1 className="text-gray-700 font-bold text-lg">{route.nombreRuta}</h1></div>
+                                    <div>Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
+                                    <div>Vuelta <hr style={{"height":"5px", "background":route.colorVuelta}}/></div>
                                 </div>
                         </CSSTransition>
                     )
