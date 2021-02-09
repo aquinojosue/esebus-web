a891d5251286b796d575fbad69385f4327b9d72f
Josué Aquino <aquinojosue3@gmail.com>
2021-02-09 02:55:31 +0000

    final changes on padding of map legend (>=xl)

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,8 +5,10 @@ function Card(props) {
         (props.routes.length > 0)?
             <div className="absolute hidden xl:block max-w-md w-full bottom-5 right-5 z-10">
                 <div className=" bg-white rounded-xl p-4 flex flex-col h-48 shadow-2xl">
-                    <div className="mb-8">
-                    <div className="text-esebus-dark font-bold text-xl mb-2">Leyenda</div>
+                    <div className="mb-8 ml-5">
+                        <div className="text-esebus-dark font-bold text-xl mb-2">
+                            Leyenda
+                        </div>
                         <div className=" overflow-auto h-28 ">
                             <RouteInfo routes={props.routes}/>
                         </div>
