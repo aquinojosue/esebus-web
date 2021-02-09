183613d811cb8df11ed160298e8b76f41bbe6dbc
Josué Aquino <aquinojosue3@gmail.com>
2021-02-09 23:25:39 +0000

    removed title from legend (>x=l)

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import RouteInfo from './RouteInfo'
-
+import {CSSTransition} from 'react-transition-group';
 
 function Card(props) {
     return (
-        (props.routes.length > 0)?
-            <div className="absolute hidden xl:block max-w-md w-full bottom-5 right-5 z-10">
-                <div className=" bg-white rounded-xl p-4 flex flex-col h-48 shadow-2xl">
-                    <div className="mb-8 ml-5">
-                        
-                        <div className=" overflow-auto h-40 ">
-                            <div className="text-esebus-dark font-bold text-xl mb-2">
-                            Leyenda
-                        </div>
-                            <RouteInfo routes={props.routes}/>
+            <CSSTransition
+                in={props.routes.length > 0}
+                timeout={300}
+                classNames="item"
+                unmountOnExit
+            >
+                <div className="absolute hidden xl:block max-w-md bottom-5 right-5 z-10">
+                    <div className=" bg-white rounded-xl p-4 flex flex-col h-32 w-72 shadow-2xl">
+                        <div className="mb-8 ml-5">
+                            <div className=" overflow-auto h-24 ">
+                                {/**
+                                <div className="text-esebus-dark font-bold text-xl mb-2">
+                                    Leyenda
+                                </div> */}
+                                <RouteInfo className="" routes={props.routes}/>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        :""
+            </CSSTransition>
     );
 }
 
