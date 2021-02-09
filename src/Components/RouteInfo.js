dfa487daf2a15f52c29e2e675df5633c4345a43d
Josué Aquino <aquinojosue3@gmail.com>
2021-02-09 23:25:51 +0000

    reformat coute (RouteInfo)

--- src/Components/RouteInfo.js
+++ src/Components/RouteInfo.js
@@ -6,19 +6,17 @@ import {
 function RouteInfo(props){
     return(
             <TransitionGroup>
-            {
-                props.routes.map((route, index)=>{
-                    return(
-                        <CSSTransition timeout={(index + 1)*300} classNames="item">
-                                <div class="bg-white border border-gray-500 p-3 mb-2 shadow-lg" key={index}>
-                                    <div><h1 className="text-gray-700 font-bold text-lg">{route.nombreRuta}</h1></div>
-                                    <div className="text-esebus-dark font-semibold">Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
-                                    <div className="text-esebus-dark font-semibold">Vuelta <hr style={{"height":"5px", "background":route.colorVuelta}}/></div>
-                                </div>
-                        </CSSTransition>
-                    )
-                })
-            }
+                {props.routes.map((route, index)=>
+                    <CSSTransition timeout={(index + 1)*300} classNames="item">
+                        <div class="bg-white border border-gray-500 p-1 mb-2 shadow-lg grid grid-rows-2 grid-flow-col" key={index}>
+                            <div className="row-span-2 w-32">
+                                <h1 className="text-gray-700 text-center font-bold text-lg">{route.nombreRuta}</h1>
+                            </div>
+                            <div className="text-esebus-dark font-semibold">Ida <hr style={{"height":"5px", "background":route.colorIda}}/></div>
+                            <div className="text-esebus-dark font-semibold">Vuelta <hr  style={{"height":"5px", "background":route.colorVuelta}}/></div>
+                        </div>
+                    </CSSTransition>
+                )}
             </TransitionGroup> 
     );
 }
